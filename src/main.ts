8fc739ea9d38f84d5e2085540b29873342bbbfdd
GalaxyGamingBoy <xrteach@gmail.com>
2023-12-15 21:30:11 +0000

    feat: Add first code!

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
-console.log("Hello World!");
+import Fastify from "fastify";
+import dotenv from "dotenv";
+
+dotenv.config();
+const fastify = Fastify({
+    logger: true,
+});
+
+fastify.get(
+    "/api",
+    {
+        schema: {
+            response: {
+                200: {
+                    type: "object",
+                    required: ["msg"],
+                    properties: { msg: { type: "string" } },
+                },
+            },
+        },
+    },
+    async () => {
+        return { mssg: "Hello, World!" };
+    }
+);
+
+try {
+    await fastify.listen({ port: Number(process.env.PORT) || 3000 });
+} catch (e) {
+    fastify.log.error(`An error occured on fastify startup!\n${e}`);
+    process.exit(1);
+}
